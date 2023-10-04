6f1c3540ac0468fc02768c5525fca662a5a68ee9
Ying WANG <ying.wang@grafana.com>
2023-10-04 14:44:29 +0200

    add more algorithm

--- server/server2.3.go
+++ server/server2.3.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"sync/atomic"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -22,6 +23,7 @@ var (
 		Name: "http_requests_total",
 		Help: "Total number of HTTP requests",
 	})
+	inflightPushRequests atomic.Int64
 )
 
 func main() {
@@ -30,23 +32,41 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Check if the request is allowed by the rate limiter.
 		requestCounter.Inc()
-
 		fmt.Println("Request received")
 		if !limiter.Allow() {
 			// Calculate the time to wait before retrying based on the rate limit.
 			// Generate jitter following a normal distribution
-			delaySeconds := 30 // Cap the base delay to 30 seconds
+			failedRequestCount := inflightPushRequests.Load() // Cap the base delay to 30 seconds
+			fmt.Println("the failedRequestCount is", failedRequestCount)
+			if failedRequestCount < 0 {
+				failedRequestCount = 0
+			}
+
+			delaySeconds := float64(failedRequestCount) * 0.1
+
 			// Add a random jitter between -0.7*delaySeconds and +0.7*delaySeconds.
-			jitter := int(rand.Float64() * float64(delaySeconds) * 0.7)
+			if delaySeconds < 5 {
+				// set minimum delay to 1 second
+				delaySeconds = 5
+			} else if delaySeconds > 30 {
+				delaySeconds = 30
+			}
+
+			jitter := (rand.Float64()*2.0 - 1.0) * delaySeconds
 			delaySeconds += jitter
-			waitTime := time.Duration(delaySeconds) * time.Second
+			waitTime := time.Duration(int64(delaySeconds)) * time.Second
 
 			// Set the "Retry-After" header with the time to wait in seconds.
 			w.Header().Set("Retry-After", fmt.Sprintf("%.0f", waitTime.Seconds()))
 
 			// Return a "Too Many Requests" (HTTP 429) status code.
 			http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
+			inflightPushRequests.Add(1)
 			return
+		} else {
+			if inflightPushRequests.Load() > 0 {
+				inflightPushRequests.Add(-1)
+			}
 		}
 
 		// Handle your request logic here.
